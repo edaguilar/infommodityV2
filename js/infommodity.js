1d4700d7ace35fa39c5b8c63c34766b4c9ad84a1
unknown <eaguilar@softelligence.com.mx>
2014-10-11 14:59:53 -0500

    Liberacion

--- js/infommodity.js
+++ js/infommodity.js
@@ -1797,7 +1797,7 @@ function terminoCargando() {
 //______________________________________________________________________
 
 // handle GCM notifications for Android
-function NotificationGCM(e) {
+function onNotificationGCM(e) {
 	//alert("onNotificationGCM");
     switch( e.event )
     {
